00a4e5ff7c9e0e6d822c0be139c6298c8f8cef78
Felipe Gutierrez <1560@holbertonschool.com>
2021-09-17 18:52:30 -0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :categories
+  devise_for :users
+  resources :links
+  #get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'links#index'
+  get 'home/about'
+end
